b0cd3d8c79bed825b05dc518a11bc8603b1fbe3d
yukiko <yukiko@magnolia.co.jp>
2025-03-30 11:14:49 +0900

    add:Model作成

---
+++ src/app/Models/Restaurant.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'area_id',
+        'genre_id',
+        'name',
+        'description',
+        'image_url',
+    ];
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
+}
